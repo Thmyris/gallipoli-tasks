2a270b19da69433edb2fa015a5f3b969b18ebd18
root <root@ip-172-31-16-176.eu-central-1.compute.internal>
2024-06-29 16:17:02 +0000

    added/fixed rxss header

--- rxss/header/index.php
+++ rxss/header/index.php
@@ -1,33 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="style.css" />
-  <title>Header Injection</title>
-</head>
-
-<body>
-  <div class="container">
-    <div>
-      <h1><a href="../../index.html" style="color: black;">< Back </a></h1>
-    </div>
-    <h1>Header Injection in GET Request</h1>
-    <div>
-    <form method="GET" action="">
-        <label for="input">Girdi: </label>
-        <input type="text" id="input" name="input">
-        <button type="submit">Gönder</button>
 <?php
-if (isset($_GET['input'])) {
-    $input = $_GET['input'];
-    echo $input;
-}
-?>
-    </form>
-    </div>
-  </div>
-</body>
+header('Content-Type: text/html');
 
+// Get the User-Agent header
+$user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reflected XSS Example</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <p>Your User-Agent is: <?php echo $user_agent; ?></p>
+</body>
 </html>
