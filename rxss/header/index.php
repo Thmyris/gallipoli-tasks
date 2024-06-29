89872e79fcd6ed8a5ea665194956a71393ff6614
a-abakus <abdullatifkurkcu@gmail.com>
2024-06-29 23:21:02 +0300

    added/fixed rxss header

--- rxss/header/index.php
+++ rxss/header/index.php
@@ -1,16 +1,31 @@
 <?php
-header('Content-Type: text/html');
-
-// Get the User-Agent header
 $user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title>Reflected XSS Example</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>XSS Header Injection</title>
 </head>
+
 <body>
-    <h1>Welcome</h1>
-    <p>Your User-Agent is: <?php echo $user_agent; ?></p>
+    <div class="container">
+        <div>
+            <h1><a href="../../index.html" style="color: black;">
+                    < Back </a>
+            </h1>
+        </div>
+        <div>
+            <h1>XSS Header Injection</h1>
+        </div>
+        <div>
+            <h4>Your User-Agent is: <?php echo $user_agent; ?></h4>
+        </div>
+    </div>
 </body>
+
 </html>
