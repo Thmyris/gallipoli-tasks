5733d071eacfbe529724587fd7b1c270ad38f612
Thmyris <thmyris@windowslive.com>
2024-07-17 17:15:31 +0000

    polyglot finished

--- task2/polyglot/index.php
+++ task2/polyglot/index.php
@@ -1,18 +1,6 @@
 <?php
-function filterXSS($input) {
-    $filtered = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', "", $input);
-    $filtered = preg_replace('/on\w+\s*=\s*"(?:.|\n)*?"/i', '', $filtered);
-    $filtered = preg_replace('/data:\s*[^\s]*?base64[^\s]*/i', '', $filtered);
-    $filtered = str_replace(['javascript:', 'expression('], '', $filtered);
-    $filtered = str_replace(['img', 'svg', 'iframe', 'eval', 'onload'], '', $filtered);
-    return $filtered;
-}
-$userInput = isset($_GET['bio']) ? $_GET['bio'] : '';
-function unsafeEcho($str) {
-    echo filterXSS($str);
-}
+  $userInput = isset($_GET['input']) ? $_GET['input'] : '';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,12 +13,57 @@ function unsafeEcho($str) {
     <div>
       <h1><a href="../index.html" style="color: black;">< Back </a></h1>
     </div>
-  <form action="" method="GET">
-      <input type="text" name="bio" required>
-      <button type="submit">Submit</button>
-  </form>
-  <div>
-    <?php echo htmlspecialchars(unsafeEcho($userInput)); ?>
-  </div>
+    <form action="" method="GET">
+        <input type="text" name="input" required>
+        <button type="submit">Submit</button>
+    </form>
+    <div>
+    <?php
+      function filter1($input) {
+        $filtered = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', "", $input);
+        $filtered = preg_replace('/on\w+\s*=\s*"(?:.|\n)*?"/i', '', $filtered);
+        $filtered = preg_replace('/data:\s*[^\s]*?base64[^\s]*/i', '', $filtered);
+        $filtered = str_replace(['javascript:', 'expression('], '', $filtered);
+        $filtered = str_replace(['iframe', 'eval', 'onload', 'img', 'onclick'], '', $filtered);
+        return $filtered;
+      }
+      function echo1($str) {
+        echo filter1($str);
+      }
+        echo "<p>1 " .  echo1($userInput) . "</p>";
+      ?>
+    </div>
+    <div>
+    <?php
+      function filter2($input) {
+        $filtered = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', "", $input);
+        $filtered = preg_replace('/on\w+\s*=\s*"(?:.|\n)*?"/i', '', $filtered);
+        $filtered = preg_replace('/data:\s*[^\s]*?base64[^\s]*/i', '', $filtered);
+        $filtered = str_replace(['javascript:', 'expression('], '', $filtered);
+        $filtered = str_replace(['iframe', 'eval', 'onmouseover', 'img', 'textarea', 'onclick'], '', $filtered);
+        return $filtered;
+      }
+      function echo2($str) {
+        echo filter2($str);
+      }
+        echo "<p>2 " .  htmlspecialchars(echo2($userInput), ENT_QUOTES) . "</p>";
+    ?>
+    </div>
+    <div>
+    <?php
+      function filter3($input) {
+        $filtered = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', "", $input);
+        $filtered = preg_replace('/on\w+\s*=\s*"(?:.|\n)*?"/i', '', $filtered);
+        $filtered = preg_replace('/data:\s*[^\s]*?base64[^\s]*/i', '', $filtered);
+        $filtered = str_replace(['expression('], '', $filtered);
+        $filtered = str_replace(['iframe', 'eval', 'onload', 'onmouseover'], '', $filtered);
+        return $filtered;
+      }
+      function echo3($str) {
+        echo filter3($str);
+      }
+        echo "<p>3 " .  echo3($userInput) . "</p>";
+    ?>
+    </div>
 </body>
 </html>
