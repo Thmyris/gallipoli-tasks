80d0039d1a101a852e13af4216ce9c1d5cda24f0
Thmyris <thmyris@windowslive.com>
2024-07-17 17:16:17 +0000

    noscript finished

--- task2/noscript/index.php
+++ task2/noscript/index.php
@@ -1,54 +1,27 @@
-<?php
-$userInput = isset($_GET['message']) ? $_GEt['message'] : '';
-function filterXSS($input) {
-    $filtered = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-    $filtered = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', '', $filtered);
-    return $filtered;
-}
-
-function filterNoscriptContent($input) {
-    return $input;
-}
-
-function echoFiltered($str, $isNoscript = false) {
-    if ($isNoscript) {
-        echo filterNoscriptContent($str);
-    } else {
-        echo filterXSS($str);
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="style.css" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
     <title>Noscript XSS</title>
 </head>
 <body>
     <div>
-      <h1><a href="../index.html" style="color: black;">< Back </a></h1>
+        <h1><a href="../index.html" style="color: black;">< Back </a></h1>
     </div>
 
-    <?php if (!empty($userInput)): ?>
-        <div>
-            <noscript>
-                <?php echoFiltered($userInput, true); ?>
-            </noscript>
-            <script>
-                document.write("<?php echo str_replace('"', '\\"', filterXSS($userInput)); ?>");
-            </script>
-        </div>
-    <?php endif; ?>
-
     <form action="" method="GET">
-      <input type="text" name="message" required />
-      <button type="submit">Submit</button>
+        <input type="text" name="message" required />
+        <button type="submit">Submit</button>
     </form>
     <div>
-      <?php echo htmlspecialchars($userInput); ?>
+        <noscript>
+        <?php       # </noscript><script>alert()</script>
+            $userInput = isset($_GET['message']) ? $_GET['message'] : '';
+            echo $userInput;
+        ?>
+        </noscript>
     </div>
 </body>
 </html>
