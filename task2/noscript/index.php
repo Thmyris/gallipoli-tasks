0866f2307f0f3d7aa5bfdd6f9183f0f417be35af
Thmyris <thmyris@windowslive.com>
2024-07-30 23:12:50 +0000

    fixed something

--- task2/noscript/index.php
+++ task2/noscript/index.php
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comment Page</title>
     <link rel="stylesheet" href="style.css" />
-    <title>Noscript XSS</title>
 </head>
 <body>
     <div>
         <h1><a href="../index.html" style="color: black;">< Back </a></h1>
     </div>
-
-    <form action="" method="GET">
-        <input type="text" name="message" required />
+    <h1>Leave a Comment</h1>
+    <form id="commentForm">
+        <textarea id="commentInput" placeholder="Enter your comment here" rows="10" cols="50"></textarea>
         <button type="submit">Submit</button>
     </form>
-    <div>
-        <?php   # <link rel="stylesheet" href="http://evil.com/malicious.js">
-                # <link rel="stylesheet" href="http://evil.com/malicious.css">  //  @import 'javascript:alert("XSS")';
-
-            function filter($input) {
-                $filtered = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', "", $input);
-                $filtered = str_replace(['iframe', 'eval', 'onmouseover', 'img', 'textarea', 'onclick', 'svg', 'onload'], '', $filtered);
-                return $filtered;
-            }
-            $userInput = filter(isset($_GET['message']) ? $_GET['message'] : '');
-            echo $userInput;
-        ?>
+    <div id="comments">
     </div>
+
+    <script>
+        document.getElementById('commentForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            var comment = document.getElementById('commentInput').value;
+            var commentsDiv = document.getElementById('comments');
+            var commentElement = document.createElement('div');
+            commentElement.className = 'comment';
+            commentElement.innerHTML = comment;
+            commentsDiv.appendChild(commentElement);
+        });
+    </script>
 </body>
 </html>
